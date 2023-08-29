c33d5988e6ac5edf38390ea60bc81dc77bc409af
Joel Mut <joel.mut@southworks.com>
2023-08-29 15:27:38 +0200

    update skill

--- samples/javascript_nodejs/02.echo-bot/bot.js
+++ samples/javascript_nodejs/02.echo-bot/bot.js
@@ -1,25 +1,53 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const {
+    ActivityHandler,
+    MessageFactory,
+    ActivityTypes,
+} = require("botbuilder");
 
 class EchoBot extends ActivityHandler {
     constructor() {
         super();
+
+        function getResponse(context) {
+            switch (context.activity.name) {
+                case "preChat":
+                    return {
+                        hoursOfOperationStatus: "open",
+                        longestWaitTime: 300, // ms
+                        chatsInQueue: 0,
+                    };
+                case "preChatAsync":
+                    return {
+                        asyncChatStatus: "existing",
+                    };
+                case "connectToLiveAgent":
+                case "endChat":
+                default:
+                    return {};
+            }
+        }
+
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            await context.sendActivity({
+                type: ActivityTypes.Message,
+                ...getResponse(context),
+            });
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = "Hello and welcome!";
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                    await context.sendActivity(
+                        MessageFactory.text(welcomeText, welcomeText)
+                    );
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
