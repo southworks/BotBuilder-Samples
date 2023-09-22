80365c5d1dbb940777720e9333725e4e965373a9
CeciliaAvila <cecilia.avila@southworks.com>
2023-09-22 12:45:03 -0300

    add snapshots logic

--- composer-samples/javascript_nodejs/projects/max-azure-itss-fcsi-bot-main/index.js
+++ composer-samples/javascript_nodejs/projects/max-azure-itss-fcsi-bot-main/index.js
@@ -1,53 +1,64 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-if (typeof globalThis === 'undefined') {
-  global.globalThis = global;
+if (typeof globalThis === "undefined") {
+    global.globalThis = global;
 }
 
 const { getRuntimeServices } = require("botbuilder-dialogs-adaptive-runtime");
 const {
-  makeApp,
+    makeApp,
 } = require("botbuilder-dialogs-adaptive-runtime-integration-express");
 const {
-  configureManifestsEndpoint,
-  configureHealthEndpoint,
-  configureAvailabilityEndpoint,
+    configureManifestsEndpoint,
+    configureHealthEndpoint,
+    configureAvailabilityEndpoint,
 } = require("./utils/endpoint");
+const express = require("express");
+const { writeHeapSnapshot } = require("v8");
 
 (async function () {
-  try {
-    setInterval(() => {
-      const used = process.memoryUsage();
-      console.log("-----------------------");
-      for (let key in used) {
-        console.log(
-          `${key} ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`
+    try {
+        // setInterval(() => {
+        //     const used = process.memoryUsage();
+        //     console.log("-----------------------");
+        //     for (let key in used) {
+        //         console.log(
+        //             `${key} ${
+        //                 Math.round((used[key] / 1024 / 1024) * 100) / 100
+        //             } MB`
+        //         );
+        //     }
+        //     console.log("-----------------------");
+        // }, 10000);
+
+        const applicationRoot = process.cwd();
+
+        const [services, configuration] = await getRuntimeServices(
+            applicationRoot,
+            "settings"
         );
-      }
-      console.log("-----------------------");
-    }, 10000);
-    
-    const applicationRoot = process.cwd();
 
-    const [services, configuration] = await getRuntimeServices(
-      applicationRoot,
-      "settings"
-    );
+        const [app, listen] = await makeApp(
+            services,
+            configuration,
+            applicationRoot
+        );
 
-    const [app, listen] = await makeApp(
-      services,
-      configuration,
-      applicationRoot
-    );
+        app.use(express.static("public"));
+        app.get("/snapshot", async (req, res) => {
+            const date = new Date().toISOString().replace(/:/g, '.')
+            const file = writeHeapSnapshot(`public/${date}.heapsnapshot`);
+            res.json({ file });
+        });
 
-    configureHealthEndpoint(app);
-    configureAvailabilityEndpoint(app);
-    configureManifestsEndpoint(app);
+        configureHealthEndpoint(app);
+        configureAvailabilityEndpoint(app);
+        configureManifestsEndpoint(app);
 
-    listen();
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+        listen();
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
 })();
